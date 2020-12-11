9a0f90397b69d5ee1478c339a821d0e1d65310b0
martin njoroge <mertoenjosh3527@gmail.com>
2020-12-12 01:02:23 +0300

    Done with draw function, placemarker function, winner, swap_player_and_maker function

--- main.cpp
+++ main.cpp
@@ -6,14 +6,18 @@ using std::cin;
 
 char board[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
 
+char current_marker;
+int current_player;
+
 void drawBoard()
 {
+  cout << "<================>\n";
   cout <<"  "<<board[0][0]<<"  |  "<<board[0][1]<<"  |  "<<board[0][2]<<endl;
   cout<<"-----------------\n";
   cout <<"  "<<board[1][0]<<"  |  "<<board[1][1]<<"  |  "<<board[1][2]<<endl;
   cout<<"-----------------\n";
   cout <<"  "<<board[2][0]<<"  |  "<<board[2][1]<<"  |  "<<board[2][2]<<endl;
-
+  cout << "<================>\n";
 }
 
 void placeMarker(int slot)
@@ -28,11 +32,74 @@ void placeMarker(int slot)
     col = (slot % 3) - 1;
   }
 
-  cout<<row<<" , " <<col;
+  board[row][col] = current_marker;
 }
+int winner()
+{
+  for (int i=0; i<3; i++){
+    // checking rows
+    if (board[i][0] == board[i][1] && board[i][1 == board[i][2]]) return current_player;
+
+    // checking col
+    if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) return current_player;
+  }
+
+  if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) return current_player;
+  if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) return current_player;
+
+  return 0;
+}
+void swap_player_and_marker()
+{
+    if (current_marker == 'X' || current_marker == 'x') {
+      current_marker = 'Y';
+    }
+    else {
+      current_marker = 'X';
+    }
+
+    if (current_player == 1) {
+      current_player = 2;
+    }
+    else {
+      current_player = 1;
+    }
+}
+void game()
+{
+  cout << "Player one, choose your marker : "<<endl;
+  char marker_p1;
+  cin >> marker_p1;
+
+  current_player = 1;
+  current_marker = marker_p1;
+
+  drawBoard();
+
+  int player_won;
+
+  for (int i=0; i<9; i++)
+  {
+    cout << "It's player "<< current_player<< "'s turn. Enter your slot."<<endl;
+    int slot;
+    cin >> slot;
+
+    placeMarker(slot);
+
+    drawBoard();
+
+    player_won = winner();
+
+    if(player_won == 1) {cout << "Player one won! Congratulations!"<<endl; break; }
+    if(player_won == 2) {cout << "Player two won! Congratulations!"<<endl; break; }
+
+    swap_player_and_marker();
+   }
+  if (player_won == 0) cout << "That is a tie GAME!!"<<endl;
+}
+
 
 int main()
 {
-  drawBoard();
-  placeMarker(9);
+  game();
 }
